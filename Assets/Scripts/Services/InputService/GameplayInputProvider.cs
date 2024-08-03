a603c3436ca925a81c29b7704e74122dab7467cd
Samarin Daniil <daniil.samarin@matryoshka.com>
2024-08-04 05:41:23 +0700

    Добавлена заглушка класса получения ввода
    Теперь на сцене есть префаб PlayerInput который будет связывать UnityEngine.InputSystem.PlayerInput с кодом игры через GameplayInputProvider.

---
+++ Assets/Scripts/Services/InputService/GameplayInputProvider.cs
@@ -0,0 +1,30 @@
+using Presents.Inputs;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Services.InputService
+{
+	[RequireComponent(typeof(PlayerInput))]
+	public class GameplayInputProvider : MonoBehaviour, GameplayInputActions.IPlayerActions
+	{
+		public void OnMove(InputAction.CallbackContext context)
+		{
+			Debug.Log($"OnMove: {context.ReadValue<Vector2>()}");
+		}
+
+		public void OnCastSpell(InputAction.CallbackContext context)
+		{
+			Debug.Log($"OnCastSpell: {context.ReadValueAsButton()}");
+		}
+
+		public void OnSelectPreviousSpell(InputAction.CallbackContext context)
+		{
+			Debug.Log($"OnSelectPreviousSpell: {context.ReadValueAsButton()}");
+		}
+
+		public void OnSelectNextSpell(InputAction.CallbackContext context)
+		{
+			Debug.Log($"OnSelectNextSpell: {context.ReadValueAsButton()}");
+		}
+	}
+}
