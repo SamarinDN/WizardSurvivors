03a75977b0c910fa3322d7e3f157c1511e3285db
Samarin Daniil <daniil.samarin@matryoshka.com>
2024-08-06 05:33:59 +0700

    В сервис каста заклинаний внедрен сервис выбора заклинаний

--- Assets/Scripts/Services/CastSpellService/CastSpellService.cs
+++ Assets/Scripts/Services/CastSpellService/CastSpellService.cs
@@ -1,6 +1,7 @@
 using System;
 using JetBrains.Annotations;
 using Services.InputService;
+using Services.SelectSpellService;
 using UniRx;
 using Zenject;
 
@@ -12,10 +13,12 @@ namespace Services.CastSpellService
 		private readonly CompositeDisposable _disposables = new();
 
 		private readonly IPlayerInputService _playerInputService;
+		private readonly ISelectSpellService _selectSpellService;
 
-		public CastSpellService(IPlayerInputService playerInputService)
+		public CastSpellService(IPlayerInputService playerInputService, ISelectSpellService selectSpellService)
 		{
 			_playerInputService = playerInputService;
+			_selectSpellService = selectSpellService;
 		}
 
 		public void Initialize()
