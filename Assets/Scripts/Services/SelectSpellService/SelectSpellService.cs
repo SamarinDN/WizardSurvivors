0b3d1d820df0584e1325440adea08e203bcca4d6
Samarin Daniil <daniil.samarin@matryoshka.com>
2024-08-06 05:03:09 +0700

    Добавлена возможность выбирать заклинания
    Теперь сервис SelectSpellService получает информацию о заклинаниях из баланса и реагирует изменением списка заклинаний на нажатие клавиш из PlayerInputService.
    SelectSpellServiceInstaller добавлен в контекст сцены геймплея.

--- Assets/Scripts/Services/SelectSpellService/SelectSpellService.cs
+++ Assets/Scripts/Services/SelectSpellService/SelectSpellService.cs
@@ -1,6 +1,58 @@
+using System;
+using System.Collections.Generic;
+using Definitions.Spells;
+using JetBrains.Annotations;
+using Services.InputService;
+using UniRx;
+using Zenject;
+
 namespace Services.SelectSpellService
 {
-	internal class SelectSpellService
+	[UsedImplicitly]
+	internal sealed class SelectSpellService : ISelectSpellService, IInitializable, IDisposable
 	{
+		private readonly IPlayerInputService _playerInputService;
+		private readonly ReactiveCollection<SpellDefinition> _availableSpells;
+
+		private readonly CompositeDisposable _disposables = new();
+
+		public IReadOnlyReactiveCollection<SpellDefinition> AvailableSpells => _availableSpells;
+
+		public SelectSpellService(IPlayerInputService playerInputService, List<SpellDefinition> spells)
+		{
+			_playerInputService = playerInputService;
+			_availableSpells = spells.ToReactiveCollection();
+		}
+
+		public void Initialize()
+		{
+			_playerInputService.SelectNextSpell.Subscribe(MoveNextSpell).AddTo(_disposables);
+			_playerInputService.SelectPreviousSpell.Subscribe(MovePreviousSpell).AddTo(_disposables);
+		}
+
+		public void Dispose()
+		{
+			_disposables.Clear();
+		}
+
+		private void MoveNextSpell(Unit _)
+		{
+			if (_availableSpells.Count < 1)
+			{
+				return;
+			}
+
+			_availableSpells.Move(0, _availableSpells.Count - 1);
+		}
+
+		private void MovePreviousSpell(Unit _)
+		{
+			if (_availableSpells.Count < 1)
+			{
+				return;
+			}
+
+			_availableSpells.Move(_availableSpells.Count - 1, 0);
+		}
 	}
 }
--- Assets/Scripts/Services/SelectSpellService/SelectSpellServiceInstaller.cs
+++ Assets/Scripts/Services/SelectSpellService/SelectSpellServiceInstaller.cs
@@ -1,11 +1,21 @@
+using System.Collections.Generic;
+using Definitions.Spells;
+using UnityEngine;
 using Zenject;
 
 namespace Services.SelectSpellService
 {
 	public sealed class SelectSpellServiceInstaller : MonoInstaller<SelectSpellServiceInstaller>
 	{
+		[SerializeField]
+		private List<SpellDefinition> spells = new();
+
 		public override void InstallBindings()
 		{
+			Container.BindInterfacesTo<SelectSpellService>()
+				.AsSingle()
+				.WithArguments(spells)
+				.NonLazy();
 		}
 	}
 }
