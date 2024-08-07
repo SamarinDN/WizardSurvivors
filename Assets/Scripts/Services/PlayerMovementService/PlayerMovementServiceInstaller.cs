b2a5f4faf907f0d9de46b0fe2b094fca2dc871ea
Samarin Daniil <daniil.samarin@matryoshka.com>
2024-08-08 01:26:47 +0700

    PlayerMovementService. Добавлено получение параметров игрока из баланса

--- Assets/Scripts/Services/PlayerMovementService/PlayerMovementServiceInstaller.cs
+++ Assets/Scripts/Services/PlayerMovementService/PlayerMovementServiceInstaller.cs
@@ -1,11 +1,17 @@
+using Definitions.Enemies;
+using UnityEngine;
 using Zenject;
 
 namespace Services.PlayerMovementService
 {
 	public sealed class PlayerMovementServiceInstaller : MonoInstaller<PlayerMovementServiceInstaller>
 	{
+		[SerializeField]
+		private PlayerDefinition playerDefinition;
+
 		public override void InstallBindings()
 		{
+			Container.Bind<PlayerDefinition>().FromScriptableObject(playerDefinition).AsSingle();
 			Container.BindInterfacesTo<PlayerMovementService>().AsSingle().NonLazy();
 		}
 	}
