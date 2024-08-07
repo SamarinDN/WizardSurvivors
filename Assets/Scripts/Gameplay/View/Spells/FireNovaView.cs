baed67495e41434d18d77fc3862aa1c5ef434094
Samarin Daniil <daniil.samarin@matryoshka.com>
2024-08-07 07:17:45 +0700

    Кольцо огня. Реализация заклинания
    Добавлена логика заклинания.
    Добавлен код во View изменяющий объект огненного кольца на сцене.

--- Assets/Scripts/Gameplay/View/Spells/FireNovaView.cs
+++ Assets/Scripts/Gameplay/View/Spells/FireNovaView.cs
@@ -1,4 +1,8 @@
+using Gameplay.SpellLogic;
+using Gameplay.SpellLogic.FireNova;
+using UniRx;
 using UnityEngine;
+using Zenject;
 
 namespace Gameplay.View.Spells
 {
@@ -6,5 +10,23 @@ namespace Gameplay.View.Spells
 	{
 		[SerializeField]
 		private Transform novaTransform;
+
+		private IFireNovaLogicDataHolder _fireNovaLogicDataHolder;
+
+		[Inject]
+		private void Constructor(IFireNovaLogicDataHolder fireNovaLogicDataHolder)
+		{
+			_fireNovaLogicDataHolder = fireNovaLogicDataHolder;
+		}
+
+		private void Awake()
+		{
+			_fireNovaLogicDataHolder.NovaRadius.Subscribe(OnNovaRadiusChanged).AddTo(this);
+		}
+
+		private void OnNovaRadiusChanged(float radius)
+		{
+			novaTransform.localScale = new Vector3(radius, novaTransform.localScale.y, radius);
+		}
 	}
 }
